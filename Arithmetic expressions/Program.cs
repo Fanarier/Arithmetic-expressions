361f290c0c12d95415ed2edb7d4dd15a1ea8f37a
KP_349 <KP_349@FANARIER>
2022-09-16 16:30:49 +0200

    Добавьте файлы проекта.

---
+++ Arithmetic expressions/Program.cs
@@ -0,0 +1,15 @@
+﻿//Плотников Кирилл 22-ИСП-2/2 Вариант-10
+try
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.Write("Vvedite chislo x = ");
+    Double x = double.Parse(Console.ReadLine());
+    Console.Write("Vvedite chislo y = ");
+    Double y = double.Parse(Console.ReadLine());
+    Double res = Math.Pow(((x + 1) / (x - 1)), x) + 18 * x * Math.Pow(y, 2);
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine($"{res:F2}");
+    Console.ForegroundColor = ConsoleColor.Green;
+}
+catch (Exception ex)
+{ Console.WriteLine(ex.ToString()); }
